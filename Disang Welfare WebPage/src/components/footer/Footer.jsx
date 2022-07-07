2fb03dcc87f29690bc19509f715c777d1b3b9f0f
pratikraj203 <94471286+pratikraj203@users.noreply.github.com>
2022-07-07 17:27:55 +0530

    Add files via upload

---
+++ Disang Welfare WebPage/src/components/footer/Footer.jsx
@@ -0,0 +1,20 @@
+import "./footer.css"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+        <div className="fLists">
+            <ul className="fList">
+                <li className="fListItem">Boys GenSec Number</li>
+                <li className="fListItem">Girls GenSec Number</li>
+                <li className="fListItem">Warden Number</li>
+                <li className="fListItem">Girls Gaurd Number</li>
+                <li className="fListItem">Boys Gaurd Number</li>
+            </ul>
+        </div>
+        <span className="fText"></span>
+    </div>
+  )
+}
+
+export default Footer
